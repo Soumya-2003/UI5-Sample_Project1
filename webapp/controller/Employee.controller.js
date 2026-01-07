814710bbc4f9d5d7b0ecc086a5029cbeb1288cd6
Soumyadeep Mallick <soumyadeep@maventic.com>
2026-01-07 07:16:16 +0000

    controlled of employee page

---
+++ webapp/controller/Employee.controller.js
@@ -0,0 +1,60 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/Device",
+    "sap/base/Log",
+    "sap/ui/model/json/JSONModel"
+], (Controller, MessageToast, Device, Log, JSONModel) => {
+    "use strict";
+    return Controller.extend("sample.project1.controller.Employee", {
+        onInit: function () {
+            var oModel = this.getOwnerComponent().getModel("departmentModel");
+            this.getView().setModel(oModel, "departmentModel");
+
+            this.getSplitAppObj().setHomeIcon({
+                'phone': 'phone-icon.png',
+                'tablet': 'tablet-icon.png',
+                'icon': 'desktop.ico'
+            });
+            
+            var oSplitApp = this.getSplitAppObj();
+            oSplitApp.toMaster(this.createId("master"));
+            oSplitApp.toDetail(this.createId("detail"));
+
+            Device.orientation.attachHandler(this.onOrientationChange, this);
+        },
+        onPressGoToMaster: function (oEvent) {
+            var oDept = oEvent.getSource()
+                .getBindingContext("departmentModel")
+                .getObject();
+
+            this.getView().getModel("departmentModel")
+                .setProperty("/selectedDepartment", oDept);
+
+            this.getSplitAppObj().toMaster(this.createId("master2"));
+        },
+        onPressMasterBack: function () {
+            this.getSplitAppObj().backMaster();
+        },
+        onEmployeePress: function (oEvent) {
+            var oEmp = oEvent.getParameter("listItem")
+                .getBindingContext("departmentModel")
+                .getObject();
+
+            this.getView().getModel("departmentModel")
+                .setProperty("/selectedEmployee", oEmp);
+
+            this.getSplitAppObj().toDetail(this.createId("detail"));
+        },
+        getSplitAppObj: function () {
+            var oSplitApp = this.byId("SplitAppDemo");
+            if (!oSplitApp) {
+                Log.info("SplitApp object can't be found");
+            }
+            return oSplitApp;
+        }
+
+
+
+    });
+});
