103f9eb4e52528414f294eb781ea0f6149b828be
Soumyadeep Mallick <soumyadeep@maventic.com>
2026-01-11 15:30:50 +0000

    base controller added

---
+++ webapp/controller/BaseController.js
@@ -0,0 +1,82 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History",
+    "sap/ui/Device",
+    "sap/ui/core/Fragment",
+    "sample/project1/helper/themeHelper"
+], (Controller, History, Device, Fragment, themeHelper) => {
+    "use strict";
+    return Controller.extend("sample.project1.controller.BaseController", {
+        navBack: function () {
+            var oHistory = History.getInstance();
+            var sPreviousHash = oHistory.getPreviousHash();
+
+            if (sPreviousHash !== undefined) {
+                window.history.go(-1);
+            } else {
+                var oRouter = this.getOwnerComponent().getRouter();
+                oRouter.navTo("RouteView1", {});
+            }
+        },
+
+        toggleSideNav: async function (oEvent) {
+            var oButton = oEvent.getSource(),
+                oView = this.getView();
+
+            if (!this._oPopover) {
+                this._oPopover = await Fragment.load({
+                    id: oView.getId(),
+                    name: "sample.project1.view.Popover",
+                    controller: this
+                });
+                oView.addDependent(this._oPopover);
+                this._oPopover.setShowHeader(Device.system.phone);
+            }
+
+            if (this._oPopover.isOpen()) {
+                this._oPopover.close();
+            } else {
+                this._oPopover.openBy(oButton);
+            }
+        },
+
+        itemSelect: function (oEvent) {
+            var sKey = oEvent.getParameter("item").getKey();
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            if (sKey === "home") {
+                oRouter.navTo("RouteView1", {});
+            }
+            else if (sKey === "welcome") {
+                oRouter.navTo("WelcomePage", {});
+            }
+            else if (sKey === "employee") {
+                oRouter.navTo("EmployeePage", {});
+            }
+            this._oPopover.close();
+        },
+
+        openThemeMenu: function(oEvent){
+            var oView = this.getView();
+
+			if (!this._oThemeMenu) {
+				Fragment.load({
+					name: "sample.project1.view.Menu",
+					controller: this
+				}).then(function (oMenu) {
+					this._oThemeMenu = oMenu;
+					oView.addDependent(oMenu);
+					oMenu.openBy(oEvent.getSource());
+				}.bind(this));
+			} else {
+				this._oThemeMenu.openBy(oEvent.getSource());
+			}
+        },
+
+        selectTheme: function (oEvent) {
+			var sTheme = oEvent.getParameter("item").getKey();
+			themeHelper.setTheme(sTheme);
+		},
+
+
+    });
+});
